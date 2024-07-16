482ca0e84ea7f7ee7de409a32cb3b6e8396c3ed7
Sophie Hasindrae <sophiehasindrae@gmail.com>
2024-07-16 15:13:55 +0300

    Feature: Add login page and routing in app

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaEnvelope, FaLock, FaEyeSlash, FaEye } from 'react-icons/fa';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:3001/api/auth/login', { email, password });
+      const token = response.data.token;
+
+      Cookies.set('token', token, { expires: 1, path: '/' });
+      navigate('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#f3fbfa]">
+      <div className="absolute inset-0 bg-cover bg-center z-0"></div>
+
+      <div className="bg-white rounded-lg px-10 pt-8 pb-10 mb-4 relative z-10 shadow-lg w-full max-w-md leading-normal">
+        <img src="/images/logo.png" alt="Mi Tsara Logo" className="mx-auto w-48 mb-6" />
+        <form onSubmit={handleSubmit}>
+          <div className="relative mb-8">
+            <input
+              type="text"
+              id="input-group-1"
+              className="bg-white border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-[#1f81a9] focus:border-[#1f81a9] block w-full pl-10 p-2.5 scale-105 shadow-lg"
+              placeholder="Email"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+              <FaEnvelope className="text-[#1f81a9]" />
+            </div>
+          </div>
+
+          <div className="relative mb-2">
+            <div>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="input-group-2"
+                className="bg-white border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-[#1f81a9] focus:border-[#1f81a9] block w-full pl-10 p-2.5 scale-105 shadow-lg"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+              <FaLock className="text-[#1f81a9]" />
+            </div>
+            <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+              <button
+                type="button"
+                className="focus:outline-none text-gray-700"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+          </div>
+          <div className="relative">
+            <Link
+              className="inline-block align-baseline float-right text-sm text-[#1f81a9] hover:text-[#145c73]"
+              to="/reset-password"
+            >
+              Forgot Password?
+            </Link>
+          </div>
+          <div className="relative mt-10 mb-4">
+            <button
+              type="submit"
+              className="text-white bg-[#1f81a9] hover:bg-[#145c73] focus:outline-none focus:ring-4 focus:ring-[#1f81a9] font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 w-full"
+            >
+              Sign In
+            </button>
+          </div>
+          
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
