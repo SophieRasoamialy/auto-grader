5384de4e2520996e08f37463e134a7bafa3e43ef
Sophie Hasindrae <sophiehasindrae@gmail.com>
2024-07-16 17:03:36 +0300

    feat: Added Dashboard and Sidebar components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import ResetPasswordPage from './components/Auth/ResetPassword';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/reset-password" element={<ResetPasswordPage />}/>
+        <Route path="/dashboard" element={<Dashboard />}/>
       </Routes>
     </Router>
   );
---
+++ src/components/components/Sidebar.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  FaHome,
+  FaBook,
+  FaUsers,
+  FaUser,
+  FaFileAlt,
+  FaClipboardList,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import Cookies from "js-cookie";
+
+const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+    navigate("/login");
+  };
+
+  return (
+    <div className="bg-[#145c73] text-white h-screen w-60 flex flex-col fixed">
+      <div className="p-4 flex items-center justify-center">
+        <img
+          src="/images/Mi Tsara 2.png"
+          alt="Mi Tsara logo"
+          className="w-32 h-auto"
+        />
+      </div>
+      <nav className="flex-1">
+        <ul className="space-y-4">
+          <li>
+            <Link
+              to="/"
+              className="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              <FaHome className="mr-2" />
+              Tableau de bord
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/sujet"
+              className="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              <FaFileAlt className="mr-2" />
+              Examen
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/etudiants"
+              className="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              <FaUsers className="mr-2" />
+              Étudiants
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/utilisateur"
+              className="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              <FaUser className="mr-2" />
+              Utilisateur
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/matiere"
+              className="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              <FaBook className="mr-2" />
+              Matière
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/classe"
+              className="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-700 hover:text-white"
+            >
+              <FaClipboardList className="mr-2" />
+              Classe
+            </Link>
+          </li>
+        </ul>
+        <div className="mt-auto">
+          <button
+            onClick={handleLogout}
+            className="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-700 hover:text-white w-full"
+          >
+            <FaSignOutAlt className="mr-2" />
+            Se deconnecter
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
